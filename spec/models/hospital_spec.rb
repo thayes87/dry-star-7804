d1cb0f0ac9cd10af8cdababf4d1b4b62ac836041
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-26 11:37:52 -0600

    add validations to model tests

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Hospital do
-  it {should have_many :doctors}
-end
+  describe 'relationships' do
+    it {should have_many :doctors}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of :name}
+  end
+end 
